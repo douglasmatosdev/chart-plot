f9633736ad75bc8554bf5175c46ce6a9301ce38a
douglasmatosdev <douglasmatosdev@gmail.com>
2021-04-04 12:20:24 -0300

    Resolve imports

--- src/popups/ShowPopUp.js
+++ src/popups/ShowPopUp.js
@@ -1,29 +1,29 @@
-import React from "react";
-import Warper from "../Warper";
-import Popup from "reactjs-popup";
-import Json from "../Json.json" // importando o modelo de JSON para ser usado para plotar um grafico
-import './style.css'
+import React from 'react';
+import Warper from '../Warper';
+import Popup from 'reactjs-popup';
+import Json from '../Json' // importando o modelo de JSON para ser usado para plotar um grafico
+import '../index.css'
 const txtJson =JSON.stringify(Json, null, 1, 't');
 
 const contentStyle = {
-  maxWidth: "500px",
-  width: "100%"
+  maxWidth: '500px',
+  width: '100%'
 };
 
 const CustomModal = () => (
   <Popup
-    trigger={<button className="button"> Generate chart </button>}
+    trigger={<button className='button'> Generate chart </button>}
     modal
     contentStyle={contentStyle}
   >
 
   {close => (
-    <div className="modal">
-      <a href="true" className="close" onClick={close}> &times; </a>
+    <div className='modal'>
+      <a href='true' className='close' onClick={close}> &times; </a>
 
-      <div className="header"> Please copy and paste this code into the console to plot a graph. </div>
-      <div className="content" >
-        {" "}
+      <div className='header'> Please copy and paste this code into the console to plot a graph. </div>
+      <div className='content' >
+        {' '}
         <textarea  style={contentStyle} defaultValue={txtJson}  readOnly rows='30' cols='50'>
           
         </textarea>
